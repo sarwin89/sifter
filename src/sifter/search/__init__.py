5ca924b9220a20f0e2d4893f79b8da669fd2d779
Sarwin Deon Chandran <sarwindeon@gmail.com>
2026-09-04 15:05:21 +0530

    feat: define staged search policies

---
+++ src/sifter/search/__init__.py
@@ -0,0 +1,5 @@
+"""Staged candidate-search policies and planning utilities."""
+
+from sifter.search.policy import SearchMode, SearchPolicy, search_policy
+
+__all__ = ["SearchMode", "SearchPolicy", "search_policy"]
