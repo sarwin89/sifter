75f4172f5e5e726dfb5f611639d55e620ed34bde
Sarwin Deon Chandran <sarwindeon@gmail.com>
2026-09-05 16:28:10 +0530

    chore: bump version to 0.2.1

--- tests/test_package.py
+++ tests/test_package.py
@@ -5,4 +5,4 @@ def test_distribution_and_namespace_are_importable() -> None:
     import sifter
 
     assert version("sifter") == sifter.__version__
-    assert sifter.__version__ == "0.2.0"
+    assert sifter.__version__ == "0.2.1"
